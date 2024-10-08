ca3fc9ecc3ea56d7b1bf11c03be105263078c8f0
hxnning <106799256+3nine@users.noreply.github.com>
2024-10-08 10:33:48 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,20 @@
+#include <WiFi.h>
+#include <WebServer.h>
 
+WebServer server(80)
+
+const char *ssid = "ESP32";
+const char *password = "12345678";
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.softAP(ssid, password);
+
+  server.begin();
+  Serial.println("Webserver gestartet, IP: " + WiFi.softAPIP().toString());
+}
+
+void loop() {
+  server.handleClient();
+}
